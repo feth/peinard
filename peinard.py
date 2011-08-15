7a9c916f191bc8fc59433ce523a5a3360b49a8ac
Feth AREZKI <feth@tuttu.info>
2011-08-15 22:55:11 +0200

    license and AUTHORS - specify alexis

--- peinard.py
+++ peinard.py
@@ -1,4 +1,28 @@
 """
+PEINARD: chill out and let me do your balance and accounting.
+
+
+This is just a small algorithm.
+
+
+Copyright (C) 2010-2011
+Created by Feth AREZKI <feth AT tuttu.info>
+Contributions by Alexis Metaireau <alexis AT notmyidea org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 api:
     call heuristic(totals)
 
