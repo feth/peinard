4c954744317a0b809ec00c8fe621b27b805cccae
Alexis Metaireau <alexis@notmyidea.org>
2011-08-15 22:30:06 +0200

    package it as "peinard"

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='peinard',
+    version='0.1',
+    description="Algorithm to balance debts and credits with the least exchanges.",
+    author='Feth Arezki',
+    author_email='feth@tuttu.info',
+    maintainer = 'Alexis Metaireau',
+    maintainer_email= 'alexis@notmyidea.org',
+    license='AGPL',
+    url='http://github.com/feth/peinard',
+    py_modules=['peinard'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
